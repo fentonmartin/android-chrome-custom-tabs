b38331b5f8cbd802fd33dfac536b71657fbba610
FEN <fen.official@gmail.com>
2016-05-18 05:57:38 +0700

    ChromeCustomTabs 1

---
+++ app/src/main/java/fen/code/chromecustomtabs/MainActivity.java
@@ -0,0 +1,17 @@
+//  FEN Chrome Custom Tab
+//  Based on :
+//  https://github.com/GoogleChrome/custom-tabs-client
+
+package fen.code.chromecustomtabs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
